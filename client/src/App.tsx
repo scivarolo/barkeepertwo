b2ee881390d97e4a6c9fd34a61b3174500baedb4
Sebastian Civarolo <scivarolo@gmail.com>
2024-06-23 18:08:59 -0500

    install react-router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,6 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 import { Button } from "@/components/ui/button";
+import { Outlet } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,12 +8,6 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
         <Button>Hey</Button>
       </div>
       <h1>Vite + React</h1>
@@ -30,6 +22,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Outlet />
     </>
   );
 }
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Landing from "./Landing.tsx";
+import Login from "./Login.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const authenticatedRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { index: true, element: <Landing /> },
+      { path: "test", element: <div>test</div> },
+    ],
+  },
+]);
+const authenticated = true;
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    {authenticated ? (
+      <RouterProvider router={authenticatedRouter} />
+    ) : (
+      <Login />
+    )}
+  </React.StrictMode>
+);
