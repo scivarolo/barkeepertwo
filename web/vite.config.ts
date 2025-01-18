20aa3fddee9f91a835c2ddd0090c1ff94bc045f7
Sebastian Civarolo <scivarolo@gmail.com>
2025-01-18 13:44:49 -0600

    https on dev server

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,12 +3,14 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 // import checker from "vite-plugin-checker";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import mkcert from "vite-plugin-mkcert";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     TanStackRouterVite(),
     react(),
+    mkcert(),
     // checker({
     //   typescript: true,
     //   // eslint: {
