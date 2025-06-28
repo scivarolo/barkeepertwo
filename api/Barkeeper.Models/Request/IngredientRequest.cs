916bfc827f84f8de22bcb3535b5132d554918878
Sebastian Civarolo <scivarolo@gmail.com>
2025-06-27 19:41:33 -0500

    .net 9

--- api/Barkeeper.Models/Request/IngredientRequest.cs
+++ api/Barkeeper.Models/Request/IngredientRequest.cs
@@ -2,7 +2,7 @@ namespace Barkeeper.Models.Request;
 
 public class IngredientRequest {
     public int Id { get; set; }
-    public string Name { get; set; }
-    public string CreatedById { get; set; }
+    public string? Name { get; set; }
+    public string? CreatedById { get; set; }
     public int? IngredientTypeId { get; set; }
 }
